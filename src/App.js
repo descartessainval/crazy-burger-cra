e1e6c54d5d950db39ebd9dab3b2224900cee3a3b
Descartes <decartes.saianval@gmail.com>
2023-08-05 02:23:38 +0200

    App.js > Add my firstname

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Salut Berta
+         Salut Descartes
         </p>
         <a
           className="App-link"

968d076280480a6d496a8406901a61deb6345472
Descartes <decartes.saianval@gmail.com>
2023-08-19 22:48:45 +0200

    ✅ F03: ORDERPAGE>Navbar>logoSmall>create logoSmall component

--- src/components/pages/reusable-ui/LogoSmall.js
+++ src/components/pages/reusable-ui/LogoSmall.js
@@ -4,7 +4,7 @@ import { theme } from '../../../themes';
 
 const LogoSmall = () => {
     return (
-        <LogoStyled >
+        <LogoStyled>
             <h1>CRAZEE</h1>
             <img src="/images/logo-orange.png" alt="log" />
             <h1>BURGER</h1> 
@@ -16,22 +16,23 @@ const LogoStyled = styled.div`
   display: flex;
   align-items: center;
   background: none;
+  margin-left:20px ;
+
 
   h1{
     display: inline;
     text-align: center;
     color: ${theme.colors.primary};
-    font-size: ${theme.fonts.size.P4}px;
+    font-size: ${theme.fonts.size.P4};
     line-height: 1em;
     font-weight: ${theme.fonts.weights.bold};
     text-transform:uppercase;
-    letter-spacing: 1.5px;
+    letter-spacing: 3px;
     font-family: "Amatic SC", cursive;
   }
   img{
     object-fit: contain;
     object-position: center;
-    height: 60px;
     width: 80px;
     margin: 0 5px;
   }

37bd3032620ceb667a0b87411dc5c8c027ff34da
Descartes <decartes.saianval@gmail.com>
2023-08-19 22:51:00 +0200

    🎨 F04: ORDERPAGE>navabar> restructure code

--- src/components/pages/navbar/Navbar.js
+++ src/components/pages/navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { styled } from 'styled-components';
-import RightSideNavbar from './RightSideNavbar';
 import { Link } from 'react-router-dom';
+import { theme } from '../../../themes';
+import RightSideNavbar from './RightSideNavbar';
 import LogoSmall from '../reusable-ui/LogoSmall';
 
 const Navbar = ({ username }) => {
@@ -14,10 +15,14 @@ const Navbar = ({ username }) => {
 }
 
 const NavbarStyled = styled.header`
-    background: red ;
     display: flex;
     justify-content: space-between;
-    width: 1400px;
+    align-items: center;
+
+    width: /*1400px*/87.5rem;
+    height: /*98.19px*/ 6.137rem ;
+    border-radius:  15px 15px 0 0;
+    background: ${theme.colors.white};
 
     a{
       text-decoration: none;

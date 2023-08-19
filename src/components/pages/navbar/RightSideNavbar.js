01093015051f2b42d3fca3287ff59cdca4cf935f
Descartes <decartes.saianval@gmail.com>
2023-08-19 22:19:35 +0200

    💄F04: ORDERPAGE>navbar>rightSidebar> add styled component and css

--- src/components/pages/navbar/RightSideNavbar.js
+++ src/components/pages/navbar/RightSideNavbar.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import SecondaryButton from '../reusable-ui/SecondaryButton';
+import { Link} from 'react-router-dom';
 import { BsPersonCircle } from 'react-icons/bs';
 import { styled } from 'styled-components';
+import { theme } from '../../../themes';
 
-const RightSideNavbar = ({ username }) => {
-    const navigate = useNavigate();
-
+const RightSideNavbar = ({ username}) => {
+    
+    const deleteLocal =()=>{
+        localStorage.clear() 
+    }
     return (
         <RightSideNavbarStyled >
             <div className='account-name'>
-                <p>Bonjour {username}</p>
-                <SecondaryButton type={"submit"} label={"Déconnexion"} handleClick={() => { navigate("/"); localStorage.clear() }} />
+                <h1>Hey, <span>{username}</span></h1>
+                <Link to={"/"} onClick={deleteLocal}  >Se déconnecter</Link>
             </div>
             <BsPersonCircle className='icon' />
         </RightSideNavbarStyled>
@@ -19,13 +21,39 @@ const RightSideNavbar = ({ username }) => {
 }
 
 const RightSideNavbarStyled = styled.div`
-    border: yellow 1px solid;
     display: flex;
-  
-  .account-name{
-    background: green
-  }
-  
+    justify-content: center;
+    align-items: center;
+    color: ${theme.colors.greyMedium};
+
+    .account-name{
+      color: ${theme.colors.white};
+      margin: 1rem;
+    }
+
+    .icon{
+        width: 36px;
+        min-height: 36px ;
+        margin: 31.09px 70px 31.09px 0;
+    }
+
+    h1, a{
+        font-weight: ${theme.fonts.weights.regular};
+        color: ${theme.colors.greyMedium};
+    }
+
+    h1{
+        margin: 0;
+        font-size: ${theme.fonts.size.P0};
+    }
+
+    h1 span{
+       color: ${theme.colors.primary};
+    }
+
+    a:hover{
+        border-bottom: 1px solid ;
+    }
 `;
 
 export default RightSideNavbar

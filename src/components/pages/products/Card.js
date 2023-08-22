02dc2895f388b9fcd0bcb0910c847b09dfca0ab3
Descartes <decartes.saianval@gmail.com>
2023-08-22 18:33:46 +0200

    ✨ F05: CARDS > Card > add component Card and logics

---
+++ src/components/pages/products/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <div className="product">
+            <img src="" alt="" />
+            <h3>Nom du produit</h3>
+            <div className="info_product">
+                <p>Prix du produit</p>
+                <button type="button">Ajouter</button>
+            </div>
+        </div>
+  )
+}
+
+export default Card

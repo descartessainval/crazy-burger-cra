7c5a40e2b39e6756208993f19522693173600a82
Descartes <decartes.saianval@gmail.com>
2023-08-28 16:13:06 +0200

    💄 F05: ORDER > Main > fix scroll bar

--- src/components/pages/order/main/Main.js
+++ src/components/pages/order/main/Main.js
@@ -15,27 +15,25 @@ const Main = () => {
 }
 
 const MainStyled = styled.main`
-
   flex: 1;
-  /*
-    //handle position with flex 
-    display: flex;
-    justify-content: center;
-  */
  
-  display: grid;
-  grid-template-columns: 1fr;
   background: ${theme.colors.background_white};
   border-bottom-right-radius:${theme.borderRadius.extraRound};
   border-bottom-left-radius: ${theme.borderRadius.extraRound}; 
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-
   
-  /* overflow: auto; */
-overflow-y: scroll;
-  -ms-overflow-style: none;  
-  scrollbar-width: none;    
+  display: grid;
+  grid-template-columns: 1fr;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar{
+    width: 5px;
+  }
+  
+  &::-webkit-scrollbar-thumb{
+    background: ${theme.colors.greyMedium};
+    border-radius: ${theme.borderRadius.round};
+  }
 `;
 
 export default Main
--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -9,25 +9,27 @@ const Menu = () => {
     return (
         <MenuStyled>
             {products.map(({id, title, imageSource, price}) => (
-                <Card imageSource={imageSource} key={id}  infoId={id} title={title} leftDescription={formatPrice(price)} /*j'utilise le spread opérator dans ce cas on préférera la premièr eméthode {...product}*//>
+                <Card
+                 imageSource={imageSource} 
+                 key={id} 
+                 infoId={id} 
+                 title={title} 
+                 leftDescription={formatPrice(price)} /*j'utilise le spread opérator dans ce cas on préférera la premièr eméthode {...product}*/
+                />
             ))}
         </MenuStyled>
     )
 }
 
 const MenuStyled = styled.ul`
-
     margin: 0;
     background: #F5F5F7;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-row-gap: 60px;
-    list-style-type:none;
     justify-items: center;
     padding: 50px 50px 150px;
-
-
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
     @media screen and  (max-width: 1120px){
         grid-template-columns: repeat(2, 1fr)
@@ -36,7 +38,6 @@ const MenuStyled = styled.ul`
     @media screen and  (max-width: 580px){
         grid-template-columns: repeat(1, 1fr)
     }
-
 `;
 
 export default Menu

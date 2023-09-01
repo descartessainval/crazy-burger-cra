5cea8a6941dce3f2aebd8da973d92d647dc8c7c8
Descartes <decartes.saianval@gmail.com>
2023-09-01 15:39:55 +0200

    🙈 F07: Add and update index.js file

--- src/components/pages/order/panel/Panel.js
+++ src/components/pages/order/panel/Panel.js
@@ -1,37 +1,52 @@
 import React, { useState } from 'react'
 import { styled } from 'styled-components';
 import { theme } from '../../../../themes';
+import { FiChevronDown, FiChevronUp } from "react-icons/fi"
+import { AiOutlinePlus } from "react-icons/ai"
+import { MdModeEditOutline } from 'react-icons/md'
+import { Link } from 'react-router-dom';
+
 
 const Panel = () => {
     const [state, setState] = useState(1)
+    const[isPanelOpen, setIsPanelOpen] = useState(false)
 
-    const action = (index) => {
-        setState(index)
+    const clicklink = (index) => {
+        setState(index);
     }
 
+    const togglePanel =()=>{
+        setIsPanelOpen(!isPanelOpen);
+    }
+    
     return (
         <PanelStyled>
             <div className="fixed-block">
                 <div className="absolute">
                     <div className="tabs">
-                        <div style={{paddingLeft:"10rem"}}></div>
-                        <div onClick={() => action(1)} className={`${state === 1 ? "tab active-tab" : "tab"}`}> chevron vers le bas </div>
-                        <div onClick={() => action(2)} className={`${state === 2 ? "tab active-tab" : "tab"}`}> Ajouter un produit  </div>
-                        <div onClick={() => action(3)} className={`${state === 3 ? "tab active-tab" : "tab"}`}> Modifier un produit </div>
-                        <div onClick={() => action(4)} className={`${state === 4 ? "tab active-tab" : "tab"}`}> Modifier un produit </div>
+                        <div style={{ paddingLeft: "10rem" }}></div>
+                        <div onClick={() => {clicklink(1); togglePanel();}} className={`${state === 1 ? "tab active-tab" : "tab"}`} >
+                           {isPanelOpen  ?  <FiChevronDown/> : <FiChevronUp/>}
+                        </div>
+                        <div onClick={() => clicklink(2)} className={`${state === 2 ? "tab active-tab" : "tab"}`}>
+                            <AiOutlinePlus/> 
+                            <p>Ajouter un produit</p> 
+                        </div>
+                        <div onClick={() => clicklink(3)} className={`${state === 3 ? "tab active-tab" : "tab"}`}>
+                            <MdModeEditOutline/>
+                            <p>Modifier un produit</p>
+                        </div>
                     </div>
                     <div className="contents">
-                        <div className={`${state === 1 ? "content active-content" : "content"}`}>
-                            <p>panel ouvert</p>
+                        <div className={`${state ===  1? "content active-content" : "content"}`}>
                         </div>
                         <div className={`${state === 2 ? "content active-content" : "content"}`}>
+                            <i></i>
                             <p>Ajouter un produit </p>
                         </div>
                         <div className={`${state === 3 ? "content active-content" : "content"}`}>
+                            <i></i>
                             <p>Modifier un produit </p>
-                            <div className={`${state === 4? "content active-content" : "content"}`}>
-                            <p>Modifier un produit </p>
-                        </div>
                         </div>
                     </div>
                 </div>
@@ -68,16 +83,17 @@ width: 87.2rem;
 }
 
 .tab, .active-tab{
-    border-top-right-radius: 10px;
-    border-top-left-radius: 10px;
-    border: 2px solid ${theme.colors.greyLight};
-
+    border-top-right-radius: 8px;
+    border-top-left-radius: 8px;
+    border: 1px solid ${theme.colors.greyLight};
+    display: flex;
+    justify-items: center;
+    align-items: center;
 }
 .tab{
     background: white;
-    padding: 1rem;
+    padding:0.5rem 1rem;
     text-align: center;
-    /* border-top: none; */
     cursor: pointer;
     position: relative;
     color: black;
@@ -103,13 +119,17 @@ width: 87.2rem;
 
 .contents{
     background: white;
-    height: 36.8vh;
+    min-height:38.65vh;
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
+
 }
 
 p{
     margin: 0;
+    font-size: 16px;
+    line-height:21.79px;
+    margin-left: 20px;
 }
 
 .content, .active-content{

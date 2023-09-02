8b1f98ee3641fef3167b29c4cdb8b3a681048dab
Descartes <decartes.saianval@gmail.com>
2023-09-02 15:36:49 +0200

    🐛 F07: LOGIN > Rename all primary_burger in primary like in index.js css

--- src/components/reusable-ui/PrimaryButton.js
+++ src/components/reusable-ui/PrimaryButton.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import { styled } from 'styled-components';
-import { theme } from '../../themes';
+  import React from 'react'
+  import { styled } from 'styled-components';
+  import { theme } from '../../themes';
 
-const PrimaryButton = ({label, Icon, className}) => {
-  return (
-    <PrimaryButtonStyled className={className}>
-      <span>{label}</span>
-      {Icon && Icon}
-    </PrimaryButtonStyled>
-  )
-}
+  const PrimaryButton = ({label, Icon, className}) => {
+    return (
+      <PrimaryButtonStyled className={className}>
+        <span>{label}</span>
+        {Icon && Icon}
+      </PrimaryButtonStyled>
+    )
+  }
 
-const PrimaryButtonStyled = styled.button`
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    white-space: nowrap;
-    text-decoration: none;
-    line-height: 1;
+  const PrimaryButtonStyled = styled.button`
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      white-space: nowrap;
+      text-decoration: none;
+      line-height: 1;
 
-    border-radius: ${theme.borderRadius.round};
-    
-    color:  ${theme.colors.white};
-    background-color:  ${theme.colors.primary_burger};
-    border: 1px solid  ${theme.colors.primary_burger};
-
-    &:hover:not([diseable]){
-        background-color:  ${theme.colors.white};
-        color:  ${theme.colors.primary_burger};
-        border:  ${theme.colors.primary_burger} 1px solid;
-        transition: all 200ms ease-out;
-    }
-
-    &:active{
+      border-radius: ${theme.borderRadius.round};
+      
       color:  ${theme.colors.white};
-      background-color:  ${theme.colors.primary_burger};
-      border: 1px solid  ${theme.colors.primary_burger};
-    }
+      background-color:  ${theme.colors.primary};
+      border: 1px solid  ${theme.colors.primary};
 
-    &:disabled{
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
-`;
+      &:hover:not([disabled]){
+          background-color:  ${theme.colors.white};
+          color:  ${theme.colors.primary};
+          border:  ${theme.colors.primary} 1px solid;
+          transition: all 200ms ease-out;
+      }
 
-export default PrimaryButton
+      &:active{
+        color:  ${theme.colors.white};
+        background-color:  ${theme.colors.primary};
+        border: 1px solid  ${theme.colors.primary};
+      }
+
+      &:disabled{
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+  `;
+
+  export default PrimaryButton

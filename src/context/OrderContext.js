a516c429d006e5081a3be366072ab46ae0873736
Descartes <decartes.saianval@gmail.com>
2023-09-10 22:34:52 +0200

    F07: ADMIN > Add conditional rendering (with  useContext)

--- src/context/OrderContext.js
+++ src/context/OrderContext.js
@@ -3,10 +3,8 @@ import {createContext} from "react"
 export default createContext({
     isModeAdmin: false,
     setIsModeAdmin: ()=>{},
-    
     state: Number,
     setState: ()=>{},
-    
     isChevronUp: false,
     setIsChevronUp: ()=>{}
 })

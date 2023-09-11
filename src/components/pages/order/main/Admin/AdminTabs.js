1a57b50e2ee1c9eeb1ea7d4e7c9ae6631082b064
Descartes <decartes.saianval@gmail.com>
2023-09-11 16:10:23 +0200

    ✨ F07: ADMIN > Tab >  Add label

--- src/components/pages/order/main/Admin/AdminTabs.js
+++ src/components/pages/order/main/Admin/AdminTabs.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { styled } from 'styled-components';
 import { FiChevronDown, FiChevronUp } from "react-icons/fi"
+import { AiOutlinePlus } from "react-icons/ai"
+// import {  } from "react-icons/ai"
 import Tab from '../../../../reusable-ui/Tab';
 import { theme } from '../../../../../themes';
 
 const AdminTabs = ({isCollapse, setIsCollapse}) => {
     return (
         <AdminTabsStyled>
-            <Tab  Icon= {isCollapse ? <FiChevronUp/> : <FiChevronDown/>}  onClick={()=>  setIsCollapse(!isCollapse)} className={isCollapse ? "is-active" : ""}/>
+            <Tab  label="" Icon= {isCollapse ? <FiChevronUp/> : <FiChevronDown/>}  onClick={()=>  setIsCollapse(!isCollapse)} className={isCollapse ? "is-active" : ""}/>
+            <Tab  label="Ajouter un produit" Icon= {<AiOutlinePlus/>}  onClick={()=>  setIsCollapse(!isCollapse)} className={isCollapse ? "is-active" : ""}/>
         </AdminTabsStyled>
     )
 }
@@ -18,11 +21,14 @@ display: flex;
 padding: 0 20px;
 
 .is-active{
-background: ${theme.colors.background_dark};
-border-color: ${theme.colors.background_dark} ;
-color: ${theme.colors.white};
+    background: ${theme.colors.background_dark};
+    border-color: ${theme.colors.background_dark} ;
+    color: ${theme.colors.white};
 }
 
+button{
+    margin-left: 1px;
+}
 `;
 
 export default AdminTabs

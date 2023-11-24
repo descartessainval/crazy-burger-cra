30faa5476018f2d01b2c363b5838db12acd9e300
Descartes <decartes.saianval@gmail.com>
2023-11-24 12:47:28 +0100

    ✅ F08: ADMIN PANEL > InputFiel > add object labelForm in tabsConfig

--- src/components/pages/order/main/Admin/tabConfig.js
+++ src/components/pages/order/main/Admin/tabConfig.js
@@ -6,13 +6,18 @@ export const tabsConfig = [
     index: "add",
     label: "Ajouter un produit",
     Icon: <AiOutlinePlus />,
+    labelForm: "Ajouter un nouveau produit au menu",
+    isValid: "Ajouté avec succès"
   },
   {
     index: "edit",
     label: "Modifier un produit",
     Icon: <MdModeEditOutline />,
+    labelForm: "Modifier un nouveau produit au menu",
+    isValid: "Modifié avec succès"
   },
 ]
+
 
 export const getTabSelected = (tabs, currentTabSelected) => {
   return tabs.find((tab) => tab.index === currentTabSelected)
